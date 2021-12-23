2c4533625f84927db4f5051a8c86805293670bb1
Martin-Floor <martin.floor@uvic.cat>
2021-12-23 08:44:57 +0100

    Added Python pacakage setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prepare_proteins",
+    version="0.0.1",
+    author="Martin Floor",
+    author_email="martinfloor@gmail.com",
+    description="A Python package to fix PDB models for further refinement",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+    'matplotlib',
+    ],
+)
