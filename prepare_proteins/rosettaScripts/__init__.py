b236f21fde2ba4bdfb08d937bab74cc9a5c734c8
Martin-Floor <martin.floor@uvic.cat>
2022-01-24 10:49:37 +0100

    Removed rosettacripts protocol.py file importing

--- prepare_proteins/rosettaScripts/__init__.py
+++ prepare_proteins/rosettaScripts/__init__.py
@@ -4,7 +4,6 @@ from .moveMapFactories import moveMapFactory
 from .selectors import residueSelectors
 from .scorefunctions import scorefunctions
 from .simpleMetrics import simpleMetrics
-from .protocols import *
 
 from .options import flags
 from .movers import movers
