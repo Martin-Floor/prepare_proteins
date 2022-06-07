31ab47cf0b55787f473a151ad1ddb0022ca3e823
Martin-Floor <martinfloor@gmail.com>
2022-06-07 13:40:00 +0200

    first tutorial

--- docs/tutorial/01-settingUpYourProteins/slurm_array.sh
+++ docs/tutorial/01-settingUpYourProteins/slurm_array.sh
@@ -2,15 +2,23 @@
 #SBATCH --job-name=AF_sequences
 #SBATCH --qos=bsc_ls
 #SBATCH --time=48:00:00
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=16
 #SBATCH --nodes=1
-#SBATCH --gres gpu:1
+#SBATCH --gres gpu:2
 #SBATCH --ntasks=1
 #SBATCH --array=1-1
+#SBATCH --constraint=k80
 #SBATCH --output=AF_sequences_%a_%A.out
 #SBATCH --error=AF_sequences_%a_%A.err
 
+module purge
+module load singularity
+module load alphafold
+
 if [[ $SLURM_ARRAY_TASK_ID = 1 ]]; then
-jobs
+cd alphafold
+Path=$(pwd)
+bsc_alphafold --fasta_paths $Path/input_sequences/GPX-Bacillus-subtilis.fasta --output_dir=$Path/output_models --model_preset=monomer_ptm --max_template_date=2022-01-01 --random_seed 1
+cd ..
 fi
 
