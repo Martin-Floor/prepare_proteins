f1649c3b4deb0afce3f58e19575cce420f4aabfe
mluengo <miguelluengo.bcn@gmail.com>
2022-08-29 16:59:30 +0200

    Remove commit test

--- prepare_proteins/_protein_models.py
+++ prepare_proteins/_protein_models.py
@@ -81,8 +81,6 @@ class proteinModels:
             method.
         """
 
-        print('hi')
-
         self.models_folder = models_folder
         self.models_paths = self._getModelsPaths()
         self.models_names = [] # Store model names
