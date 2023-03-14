b52c81c79a4df1e59adad0771b09e8f20f4ab720
Martin-Floor <martinfloor@gmail.com>
2023-03-14 14:35:19 +0100

    Change the iterationof model and ligands in combineDockingDistancesIntoMetrics

--- prepare_proteins/_protein_models.py
+++ prepare_proteins/_protein_models.py
@@ -3546,9 +3546,19 @@ make sure of reading the target sequences with the function readTargetSequences(
                 print('Combined metric %s already added. Give overwrite=True to recombine' % name)
             else:
                 values = []
-                for model in self.docking_distances:
+                for model in self.docking_data.index.levels[0]:
+
+                    # Check whether model is found in docking distances
+                    if model not in self.docking_distances:
+                        continue
+
                     model_series = self.docking_data[self.docking_data.index.get_level_values('Protein') == model]
-                    for ligand in self.docking_distances[model]:
+
+                    for ligand in self.docking_data.index.levels[1]:
+
+                        # Check whether ligand is found in model's docking distances
+                        if model not in self.docking_distances[model]:
+                            continue
 
                         ligand_series = model_series[model_series.index.get_level_values('Ligand') == ligand]
                         distances = catalytic_labels[name][model][ligand]
@@ -3577,19 +3587,9 @@ make sure of reading the target sequences with the function readTargetSequences(
         best_poses = pd.DataFrame()
         bp = []
         failed = []
-        for model in self.docking_data.index.levels[0]:
-
-            # Check whether model is found in docking distances
-            if model not in self.docking_distances:
-                continue
-
+        for model in self.docking_data:
             protein_series = self.docking_data[self.docking_data.index.get_level_values('Protein') == model]
-            for ligand in self.docking_data.index.levels[1]:
-
-                # Check whether ligand is found in model's docking distances
-                if ligand not in self.docking_distances[model]:
-                    continue
-
+            for ligand in self.docking_ligands[model]:
                 ligand_data = protein_series[protein_series.index.get_level_values('Ligand') == ligand]
                 for metric in filter_values:
 
