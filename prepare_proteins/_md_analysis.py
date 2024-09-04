4463ba3a2b3055a958483d8df9a2c4c06217e248
mluengo <miguelluengo.bcn@gmail.com>
2024-09-04 12:03:13 +0200

    more bugs in previous push

--- prepare_proteins/_md_analysis.py
+++ prepare_proteins/_md_analysis.py
@@ -135,13 +135,10 @@ class md_analysis:
 
                     # sort files in case they are higher than 10
                     file_list = [f for f in os.listdir(md_path) if f.endswith('_noPBC.xtc')]
-                    def num_sort(test_string):
-                        return list(map(int,test_string.split('_')[2]))[0]
-                    # calling function
-                    file_list.sort(key=num_sort)
-                    file_list = ' '.join([md_path+'/'+f for f in file_list])
+                    sorted_file_list = sorted(file_list, key=lambda x: int(x.split('_')[2]))
+                    sorted_file_list = ' '.join([md_path+'/'+f for f in sorted_file_list])
 
-                    os.system(f'{command} trjcat -f {file_list} -o {md_path}/prot_md_cat_noPBC.xtc -cat')
+                    os.system(f'{command} trjcat -f {sorted_file_list} -o {md_path}/prot_md_cat_noPBC.xtc -cat')
                     if remove_redundant_files:
                         [os.remove(path) for path in remove_paths]
 
