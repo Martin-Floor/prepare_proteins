af987f34a5b497b98df1378d164db442497e676d
Martin-Floor <martinfloor@gmail.com>
2025-09-13 10:06:00 +0200

    Updated for reading models in batch (not working yet)

--- prepare_proteins/__init__.py
+++ prepare_proteins/__init__.py
@@ -1,4 +1,5 @@
 from ._protein_models import proteinModels, _readPDB, _saveStructureToPDB
+from ._protein_models import make_model_path_batches
 from ._protein_models import _copyScriptFile, _readRosettaScoreFile
 from ._prepare_sequences import sequenceModels
 from ._md_analysis import md_analysis
